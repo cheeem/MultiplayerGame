287f995131cc81e94888496b249c694c5882a1c5
Jackson Werner <74575896+cheeem@users.noreply.github.com>
2024-07-15 16:37:20 -0500

    DataView For Binary Parsing, Click Events

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,22 +1,19 @@
-const ws: WebSocket = new WebSocket('ws://localhost:3000');
+const url: string = "ws://localhost:3000";
 const canvas: HTMLCanvasElement = document.querySelector("canvas")!;
 const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
-const u8_max: number = 255;
-const factor: number = 2;
-
-const footer_size: number = 2;
-
-canvas.width = u8_max * factor;
-canvas.height = u8_max * factor;
-
 const COLORS = ["red", "blue", "green", "yellow", "purple"] as const;
 
-//let buf: ArrayBuffer;
-let buf_u8: Uint8Array;
-let buf_u16: Uint16Array;
-
+let ws: WebSocket = new WebSocket(url);
+let view: DataView;
 let self_idx: number;
+
+const canvas_size: number = 255;
+// const factor: number = 2;
+const footer_size: number = 1;
+
+canvas.width = canvas_size;
+canvas.height = canvas_size;
 
 ws.binaryType = "arraybuffer";
 
@@ -25,15 +22,20 @@ ws.onopen = () => {
     render();
 }
 
+// not working
+// ws.onclose = () => {
+//     console.log("restarting")
+//     ws = new WebSocket(url);
+// }
+
 ws.onmessage = (e: MessageEvent) => {
 
     const buf: ArrayBuffer = e.data;
 
-    buf_u8 = new Uint8Array(buf);
-    buf_u16 = new Uint16Array(buf);
+    view = new DataView(buf);
 
     // footer 
-    self_idx = buf_u8[buf_u8.length-1];
+    self_idx = view.getInt8(view.byteLength-1);
 
 }
 
@@ -45,16 +47,16 @@ document.onkeydown = (e: KeyboardEvent) => {
 
     switch(e.key) {
         case("w"):
-            send_client_event(ws, 0);
+            send_key_event(ws, 0);
             break;
         case("s"):
-            send_client_event(ws, 2);
+            send_key_event(ws, 2);
             break;
         case("a"):
-            send_client_event(ws, 4);
+            send_key_event(ws, 4);
             break;
         case("d"):
-            send_client_event(ws, 6);
+            send_key_event(ws, 6);
             break;
     }
     
@@ -68,16 +70,16 @@ document.onkeyup = (e: KeyboardEvent) => {
 
     switch(e.key) {
         case("w"):
-            send_client_event(ws, 1);
+            send_key_event(ws, 1);
             break;
         case("s"):
-            send_client_event(ws, 3);
+            send_key_event(ws, 3);
             break;
         case("a"):
-            send_client_event(ws, 5);
+            send_key_event(ws, 5);
             break;
         case("d"):
-            send_client_event(ws, 7);
+            send_key_event(ws, 7);
             break;
     }
     
@@ -86,16 +88,25 @@ document.onkeyup = (e: KeyboardEvent) => {
 // we don't really need self_idx rn, could be useful in the future for some reason but i might remove it 
 // coords should be sent to the server divided by the factor, server will calculate normal vector (direction)
 
-// canvas.onclick = (e: MouseEvent) => {
+canvas.onclick = (e: MouseEvent) => {
 
-//     e.offsetX
-//     e.offsetY
+    const x: number = Math.floor(e.offsetX);
+    const y: number = Math.floor(e.offsetY);
 
-// }
+    const buf: ArrayBuffer = new ArrayBuffer(5);
+    const view: DataView = new DataView(buf);
+
+    view.setUint8(0, 8);
+    view.setUint16(1, x);
+    view.setUint16(3, y);
+    
+    ws.send(buf);
+
+}
 
 function render() {
 
-    if(!buf_u8) {
+    if(!view) {
         return requestAnimationFrame(render)
     }
 
@@ -103,19 +114,21 @@ function render() {
 
     let idx: number = 0;
 
-    while(idx < buf_u8.length - footer_size) {
+    while(idx < view.byteLength - footer_size) {
 
-        switch(buf_u8[idx]) {
+        switch(view.getUint8(idx)) {
             case(0): {
-                user(ctx, buf_u8, buf_u16, idx);
+                user(ctx, view, idx);
                 idx += 10;
                 break;
             }
             case(1): {
-                platform(ctx, buf_u8, buf_u16, idx);
+                platform(ctx, view, idx);
                 idx += 8;
                 break;
             }
+            default:
+                alert("Invalid Object Type")
         }
 
     }
@@ -124,16 +137,15 @@ function render() {
 
 }
 
-function user(ctx: CanvasRenderingContext2D, buf_u8: Uint8Array, buf_u16: Uint16Array, idx: number) {
+function user(ctx: CanvasRenderingContext2D, view: DataView, idx: number) {
     
-    const idx_u16: number = idx / 2;
-    let sprite_idx = buf_u8[idx + 1]; // change back to const
-    const animation_idx = buf_u8[idx + 2];
-    const user_idx = buf_u8[idx + 3];
-    const width = buf_u8[idx + 4] * factor;
-    const height = buf_u8[idx + 5] * factor;
-    const x = buf_u16[idx_u16 + 3] * factor;
-    const y = buf_u16[idx_u16 + 4] * factor;
+    let sprite_idx: number = view.getUint8(idx + 1); // change back to const
+    const animation_idx: number = view.getUint8(idx + 2);
+    const user_idx: number = view.getUint8(idx + 3);
+    const width: number = view.getUint8(idx + 4);
+    const height: number = view.getUint8(idx + 5);
+    const x: number = view.getUint16(idx + 6);
+    const y: number = view.getUint16(idx + 8);
 
     if(user_idx == self_idx) {
         sprite_idx = 2;
@@ -144,21 +156,20 @@ function user(ctx: CanvasRenderingContext2D, buf_u8: Uint8Array, buf_u16: Uint16
 
 }
 
-function platform(ctx: CanvasRenderingContext2D, buf_u8: Uint8Array, buf_u16: Uint16Array, idx: number) {
+function platform(ctx: CanvasRenderingContext2D, view: DataView, idx: number) {
     
-    const idx_u16: number = idx / 2;
-    const sprite_idx = buf_u8[idx + 1];
-    const width = buf_u8[idx + 2] * factor;
-    const height = buf_u8[idx + 3] * factor;
-    const x = buf_u16[idx_u16 + 2] * factor;
-    const y = buf_u16[idx_u16 + 3] * factor;
+    const sprite_idx: number = view.getUint8(idx + 1);
+    const width: number = view.getUint8(idx + 2);
+    const height: number = view.getUint8(idx + 3);
+    const x: number = view.getUint16(idx + 4);
+    const y: number = view.getUint16(idx + 6);
 
     ctx.fillStyle = COLORS[sprite_idx];
     ctx.fillRect(x, y, width, height);
 
 }
 
-function send_client_event(ws: WebSocket, byte: number) {
+function send_key_event(ws: WebSocket, byte: number) {
 
     const buf: Uint8Array = new Uint8Array(1);
 
